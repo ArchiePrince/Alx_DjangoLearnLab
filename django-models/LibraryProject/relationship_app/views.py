81cc1f702cbb700220491c020f30f7ddd2c41f9a
ArchiePrince <acheampongprince77@gmail.com>
2025-03-19 17:18:07 -0400

    Modified the  Views py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from .models import Library
 from .models import Book
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 # from django.contrib.auth.decorators import login_required
 
 # @login_required
