25302da942b4972737d8240c8a5b4dc1dc31e404
ArchiePrince <acheampongprince77@gmail.com>
2025-03-19 12:03:18 -0400

    Added and modified dirs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,12 @@
-
-
-from .models import Book
+from django.views.generic import DetailView
+from .models  import Library, Book
 from django.shortcuts import render
-
-from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
-    from .models  import Library
-    
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
